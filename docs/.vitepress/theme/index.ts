0e789db0c84b93c205ed1d13d732f7fbd861b366
plumlanguage <plumlanguage@163.com>
2024-12-29 11:49:11 +0800

    Add files via upload

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,6 +3,8 @@ import DefaultTheme from "vitepress/theme";
 
 import Confetti from "./components/Confetti.vue";
 
+import "./style/index.css"; //引入自定义的样式
+
 export default {
   extends: DefaultTheme,
   enhanceApp({ app, router }) {
